66878b927ad66d67b0df7f4fcb65e1b9c15f84cd
Nick Pentreath <nick.pentreath@gmail.com>
2019-03-12 20:28:12 +0200

    Handle invalid input and various cleanups (#18)

--- core/model.py
+++ core/model.py
@@ -4,13 +4,23 @@ import tensorflow as tf
 from tensorflow.contrib.saved_model.python.saved_model import signature_def_utils
 from tensorflow import saved_model as sm
 import logging
-from config import DEFAULT_MODEL_PATH
+from config import DEFAULT_MODEL_PATH, ERR_MSG, MODEL_NAME, MODEL_LICENSE, MODEL_ID
 
 logger = logging.getLogger()
 
 
 class ModelWrapper(MAXModelWrapper):
     """Model wrapper for TensorFlow models in SavedModel format"""
+
+    MODEL_META_DATA = {
+        'id': MODEL_ID,
+        'name': MODEL_NAME,
+        'description': 'Converts a grayscale image to a color image.',
+        'type': 'Image-To-Image Translation',
+        'license': MODEL_LICENSE,
+        'source': 'https://developer.ibm.com/exchanges/models/all/max-image-colorizer'
+    }
+
     def __init__(self, path=DEFAULT_MODEL_PATH):
         logger.info('Loading model from: {}...'.format(path))
         sess = tf.Session(graph=tf.Graph())
@@ -32,5 +42,12 @@ class ModelWrapper(MAXModelWrapper):
     def _predict(self, x):
 
         # Run prediction on input
-        preds = self.output_tensor.eval(feed_dict={self.input_name: x}, session=self.sess)
-        return preds
+        try:
+            preds = self.output_tensor.eval(feed_dict={self.input_name: x}, session=self.sess)
+            return preds
+        except tf.errors.InvalidArgumentError as e:
+            if 'Expected image' in e.message:
+                raise OSError(ERR_MSG)
+            else:
+                raise e
+
